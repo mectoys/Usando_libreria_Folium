97ca243da3a30b1926c44fed0ef36bb709211d68
mectoys <mectoys2013@gmail.com>
2023-03-15 21:44:50 -0500

    Usando_Libreria_Folium

--- scr/FoliumTutorial.py
+++ scr/FoliumTutorial.py
@@ -2,33 +2,35 @@ import os.path
 import random
 import folium
 
-
+#1
 def SaveLocation():
     dirname = os.path.dirname(__file__)
     dir = r"Mapas\foliumtutorial.html"
     savelocation = os.path.join(dirname, dir)
     return savelocation
 
-
+#2
 def DisplayWebMapTiles():
     m = folium.Map()
     m.save(SaveLocation())
 
-
+#3
 def DisplayWebMapName():
     number = random.randint(0, 3)
     Name_Tiles = ["Stamen Terrain", "Stamen Toner", "cartodb positron", "OpenStreetMap"]
     m = folium.Map(tiles="Stamen Terrain")
     m.save(SaveLocation())
-
+#4
 #latitud y longitud
 def DisplayGeolocation():
     m = folium.Map(location=(49.25, -123.12), tiles="cartodb positron")
     m.save(SaveLocation())
+#5
 #Zoom
 def DisplayMapWithZoom():
     m = folium.Map(location=(30,10),zoom_start=5, tiles="cartodb positron")
     m.save(SaveLocation())
+#6
 #GoeJsonLayer
 def DisplayGeoJSONLayer():
     political_countries_url = (
@@ -38,13 +40,13 @@ def DisplayGeoJSONLayer():
 
     folium.GeoJson(political_countries_url).add_to(m)
     m.save(SaveLocation())
-
+#7
 #marcadores
 def DisplayMarkerLayer():
     m = folium.Map(location=(100, 10), zoom_start=2, tiles="cartodb positron")
     folium.Marker(location=[34.06,-118.25]).add_to(m)
     m.save(SaveLocation())
-
+#8
 #Marcador con Icono
 def DisplayMarkerWithIcon():
     #https://fontawesome.com/v4/icon/bandcamp
@@ -52,7 +54,7 @@ def DisplayMarkerWithIcon():
     folium.Marker(location=[34.06, -118.25],icon=folium.Icon(icon="podcast",prefix='fa',color="red")).add_to(m)
 
     m.save(SaveLocation())
-
+#9
 def AdCircleMarker():
     m = folium.Map(location=(34.06, -110.25), zoom_start=8, tiles="cartodb positron")
     folium.CircleMarker(
