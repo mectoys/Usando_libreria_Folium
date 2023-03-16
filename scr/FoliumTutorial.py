c81d0de6fe2275959b39af7b3521264d813b860e
mectoys <mectoys2013@gmail.com>
2023-03-15 21:41:55 -0500

    Usando_Libreria_Folium

---
+++ scr/FoliumTutorial.py
@@ -0,0 +1,77 @@
+import os.path
+import random
+import folium
+
+
+def SaveLocation():
+    dirname = os.path.dirname(__file__)
+    dir = r"Mapas\foliumtutorial.html"
+    savelocation = os.path.join(dirname, dir)
+    return savelocation
+
+
+def DisplayWebMapTiles():
+    m = folium.Map()
+    m.save(SaveLocation())
+
+
+def DisplayWebMapName():
+    number = random.randint(0, 3)
+    Name_Tiles = ["Stamen Terrain", "Stamen Toner", "cartodb positron", "OpenStreetMap"]
+    m = folium.Map(tiles="Stamen Terrain")
+    m.save(SaveLocation())
+
+#latitud y longitud
+def DisplayGeolocation():
+    m = folium.Map(location=(49.25, -123.12), tiles="cartodb positron")
+    m.save(SaveLocation())
+#Zoom
+def DisplayMapWithZoom():
+    m = folium.Map(location=(30,10),zoom_start=5, tiles="cartodb positron")
+    m.save(SaveLocation())
+#GoeJsonLayer
+def DisplayGeoJSONLayer():
+    political_countries_url = (
+        "http://geojson.xyz/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
+    )
+    m = folium.Map(location=(30, 10), zoom_start=3, tiles="cartodb positron")
+
+    folium.GeoJson(political_countries_url).add_to(m)
+    m.save(SaveLocation())
+
+#marcadores
+def DisplayMarkerLayer():
+    m = folium.Map(location=(100, 10), zoom_start=2, tiles="cartodb positron")
+    folium.Marker(location=[34.06,-118.25]).add_to(m)
+    m.save(SaveLocation())
+
+#Marcador con Icono
+def DisplayMarkerWithIcon():
+    #https://fontawesome.com/v4/icon/bandcamp
+    m = folium.Map(location=(34.06, -110.25), zoom_start=8, tiles="cartodb positron")
+    folium.Marker(location=[34.06, -118.25],icon=folium.Icon(icon="podcast",prefix='fa',color="red")).add_to(m)
+
+    m.save(SaveLocation())
+
+def AdCircleMarker():
+    m = folium.Map(location=(34.06, -110.25), zoom_start=8, tiles="cartodb positron")
+    folium.CircleMarker(
+        location=[34.06, -118.25],
+        radius=45,
+        fill=True,
+        color="blue",
+        fill_color="red",
+        fill_opacity=0.50,
+        tooltip="Hola mapa"
+    ).add_to(m)
+    m.save(SaveLocation())
+
+if __name__ == '__main__':
+    # DisplayWebMapTiles()
+    # DisplayWebMapName()
+    #DisplayGeolocation()
+    #DisplayMapWithZoom()
+    #DisplayGeoJSONLayer()
+    #DisplayMarkerLayer()
+    #DisplayMarkerWithIcon()
+    AdCircleMarker()
